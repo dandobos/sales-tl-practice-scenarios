322265e3ddbaef21d0f0201ad15ee1a994240c44
Dan Dobos <dan@dandobos.com>
2026-06-30 15:58:15 +1000

    Remove intro work sample copy

--- app.js
+++ app.js
@@ -51,7 +51,6 @@
       <section class="intro-grid">
         <div class="intro-copy">
           <h2>Give feedback on four short scenarios</h2>
-          <p class="lede">Your responses are treated as a work sample for the Sales Team Leader role. Submit only when you are ready; responses cannot be edited afterward.</p>
           ${configWarning()}
         </div>
         <form class="panel form-panel" id="start-form">
